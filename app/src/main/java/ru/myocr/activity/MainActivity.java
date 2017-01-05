addf28499fbd8cd4c29909d23cb1417490c8efdf
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-01-05 12:48:42 +0300

    add run CamScanner listener

--- app/src/main/java/ru/myocr/activity/MainActivity.java
+++ app/src/main/java/ru/myocr/activity/MainActivity.java
@@ -2,6 +2,7 @@ package ru.myocr.activity;
 
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -25,10 +26,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.buttonRunCamScanner.setOnClickListener(v -> runCamScanner());
         binding.buttonPhoto.setOnClickListener(v -> startPhoto());
         binding.buttonCamera.setOnClickListener(v -> startCamera());
 
         initOpenCV();
+    }
+
+    private void runCamScanner() {
+        startNewActivity("com.intsig.camscanner");
     }
 
     private void startPhoto() {
@@ -45,5 +51,16 @@ public class MainActivity extends AppCompatActivity {
         if (OpenCVLoader.initDebug()) {
             Log.d(TAG, "OpenCV library found inside package. Using it!");
         }
+    }
+
+    public void startNewActivity(String packageName) {
+        Intent intent = getPackageManager().getLaunchIntentForPackage(packageName);
+        if (intent == null) {
+            // Bring user to the market or let them choose an app?
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("market://details?id=" + packageName));
+        }
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
