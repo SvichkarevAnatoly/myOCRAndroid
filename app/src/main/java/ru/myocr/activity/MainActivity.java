42782372a22ff88a7fa790094350c37c79a85895
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-01-05 14:22:51 +0300

    feature run CamScanner scanner straight

--- app/src/main/java/ru/myocr/activity/MainActivity.java
+++ app/src/main/java/ru/myocr/activity/MainActivity.java
@@ -2,7 +2,6 @@ package ru.myocr.activity;
 
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -34,7 +33,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void runCamScanner() {
-        startNewActivity("com.intsig.camscanner");
+        Intent intent = new Intent("com.intsig.camscanner.NEW_DOC");
+        startActivity(intent);
     }
 
     private void startPhoto() {
@@ -51,16 +51,5 @@ public class MainActivity extends AppCompatActivity {
         if (OpenCVLoader.initDebug()) {
             Log.d(TAG, "OpenCV library found inside package. Using it!");
         }
-    }
-
-    public void startNewActivity(String packageName) {
-        Intent intent = getPackageManager().getLaunchIntentForPackage(packageName);
-        if (intent == null) {
-            // Bring user to the market or let them choose an app?
-            intent = new Intent(Intent.ACTION_VIEW);
-            intent.setData(Uri.parse("market://details?id=" + packageName));
-        }
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
     }
 }
