90e02c53f0fd5995580c6a3ea5712589cfa01169
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-01-05 23:46:05 +0300

    feature find in data set

--- app/src/main/java/ru/myocr/model/align/DataBaseFinder.java
+++ app/src/main/java/ru/myocr/model/align/DataBaseFinder.java
@@ -3,9 +3,24 @@ package ru.myocr.model.align;
 import java.util.List;
 
 public class DataBaseFinder {
-    private final List<String> productNames;
+    private final List<String> products;
 
-    public DataBaseFinder(List<String> productNames) {
-        this.productNames = productNames;
+    public DataBaseFinder(List<String> products) {
+        this.products = products;
+    }
+
+    public String find(String ocrProduct) {
+        int bestScore = Integer.MIN_VALUE;
+        String bestScoreProduct = "";
+        for (String product : products) {
+            Aligner aligner = new SimpleAligner();
+            int alignScore = aligner.align(ocrProduct, product);
+            if (alignScore > bestScore) {
+                bestScore = alignScore;
+                bestScoreProduct = product;
+            }
+        }
+
+        return bestScoreProduct;
     }
 }
