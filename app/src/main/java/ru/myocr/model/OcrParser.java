074894a1399a03252c7278e743f619e366ee6f95
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-01-06 23:22:50 +0300

    feature delete empty lines

--- app/src/main/java/ru/myocr/model/OcrParser.java
+++ app/src/main/java/ru/myocr/model/OcrParser.java
@@ -3,6 +3,7 @@ package ru.myocr.model;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class OcrParser {
@@ -18,10 +19,28 @@ public class OcrParser {
     }
 
     public List<String> parseProductList() {
-        return new ArrayList<>(Arrays.asList(productsText.split("\\n")));
+        final List<String> products = splitByNewLine(productsText);
+        deleteEmptyStrings(products);
+        return products;
     }
 
     public List<String> parsePriceList() {
-        return new ArrayList<>(Arrays.asList(pricesText.split("\\n")));
+        final List<String> prices = splitByNewLine(pricesText);
+        deleteEmptyStrings(prices);
+        return prices;
+    }
+
+    private List<String> splitByNewLine(String text) {
+        return new ArrayList<>(Arrays.asList(text.split("\\n")));
+    }
+
+    private void deleteEmptyStrings(List<String> strings) {
+        final Iterator<String> it = strings.iterator();
+        while (it.hasNext()) {
+            final String next = it.next();
+            if (next.trim().isEmpty()) {
+                it.remove();
+            }
+        }
     }
 }
