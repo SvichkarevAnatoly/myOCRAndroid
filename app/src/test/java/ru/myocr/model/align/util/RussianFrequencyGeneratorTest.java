f32c744c62bd218c95e1b885e995eb952254f880
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-01-06 09:22:22 +0300

    test russian generator

---
+++ app/src/test/java/ru/myocr/model/align/util/RussianFrequencyGeneratorTest.java
@@ -0,0 +1,81 @@
+package ru.myocr.model.align.util;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static ru.myocr.model.align.util.RussianFrequencyGenerator.ALPHABET;
+import static ru.myocr.model.align.util.RussianFrequencyGenerator.FREQUENCIES;
+
+public class RussianFrequencyGeneratorTest {
+    @Test
+    public void getCumulativeFrequency() throws Exception {
+        final RussianFrequencyGenerator generator = new RussianFrequencyGenerator(0);
+        final int[] cumulativeFrequency = generator.getCumulativeFrequency();
+
+        printCumulativeFrequency(cumulativeFrequency);
+
+        assertThat(cumulativeFrequency[0], is(generator.getFrequency(0)));
+        assertThat(cumulativeFrequency[1], is(generator.getFrequency(0) + generator.getFrequency(1)));
+    }
+
+    @Test
+    public void getCharacter() throws Exception {
+        final RussianFrequencyGenerator generator = new RussianFrequencyGenerator(0);
+        final String alphabetStr = new String(ALPHABET);
+
+        final int[] alphabetCounters = new int[ALPHABET.length];
+        for (int i = 0; i < 100000; i++) {
+            final char randChar = generator.getChar();
+            final int indexOfChar = alphabetStr.indexOf(randChar);
+            alphabetCounters[indexOfChar]++;
+        }
+
+        printAlphabetCounters(alphabetCounters);
+
+        assertThat(maxChar(alphabetCounters), is('О'));
+        assertThat(minChar(alphabetCounters), is('Ё'));
+    }
+
+    private void printCumulativeFrequency(int[] cumulativeFrequency) {
+        for (int i = 0; i < cumulativeFrequency.length; i++) {
+            System.out.println(ALPHABET[i] + " - " +
+                    FREQUENCIES[i] + " - " + cumulativeFrequency[i]);
+        }
+    }
+
+    private void printAlphabetCounters(int[] alphabetCounters) {
+        for (int i = 0; i < alphabetCounters.length; i++) {
+            System.out.println(ALPHABET[i] + " - " + alphabetCounters[i]);
+        }
+    }
+
+    private char maxChar(int[] alphabetCounters) {
+        int max = 0;
+        int maxIndex = 0;
+
+        for (int i = 0; i < alphabetCounters.length; i++) {
+            if (alphabetCounters[i] > max) {
+                max = alphabetCounters[i];
+                maxIndex = i;
+            }
+        }
+
+        return ALPHABET[maxIndex];
+    }
+
+    private char minChar(int[] alphabetCounters) {
+        int min = Integer.MAX_VALUE;
+        int minIndex = 0;
+
+        for (int i = 0; i < alphabetCounters.length; i++) {
+            if (alphabetCounters[i] < min) {
+                min = alphabetCounters[i];
+                minIndex = i;
+            }
+        }
+
+        return ALPHABET[minIndex];
+    }
+}
