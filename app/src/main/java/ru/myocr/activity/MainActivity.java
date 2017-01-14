065b49aec0c722007fc9e917a18089d4b3ed6ce4
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-01-14 19:24:22 +0300

    refactor on activity result

--- app/src/main/java/ru/myocr/activity/MainActivity.java
+++ app/src/main/java/ru/myocr/activity/MainActivity.java
@@ -17,10 +17,8 @@ import ru.myocr.model.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "myOcr";
-
+    private static final int PICK_IMAGE_REQUEST = 1;
     private ActivityMainBinding binding;
-
-    private int PICK_IMAGE_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,20 +34,20 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // handle result of CropImageActivity
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-            CropImage.ActivityResult result = CropImage.getActivityResult(data);
-            if (resultCode == RESULT_OK) {
-                ((ImageView) findViewById(R.id.quick_start_cropped_image)).setImageURI(result.getUri());
-                Toast.makeText(this, "Cropping successful, Sample: " + result.getSampleSize(), Toast.LENGTH_LONG).show();
-            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
-                Toast.makeText(this, "Cropping failed: " + result.getError(), Toast.LENGTH_LONG).show();
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case PICK_IMAGE_REQUEST:
+                    if (data != null && data.getData() != null) {
+                        Uri imageUri = data.getData();
+                        startCropImageActivity(imageUri);
+                    }
+                    break;
+                case CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE:
+                    CropImage.ActivityResult result = CropImage.getActivityResult(data);
+                    ((ImageView) findViewById(R.id.quick_start_cropped_image)).setImageURI(result.getUri());
+                    Toast.makeText(this, "Cropping successful, Sample: " + result.getSampleSize(), Toast.LENGTH_LONG).show();
+                    break;
             }
-        } else if (requestCode == PICK_IMAGE_REQUEST
-                && resultCode == RESULT_OK
-                && data != null && data.getData() != null) {
-            Uri imageUri = data.getData();
-            startCropImageActivity(imageUri);
         }
     }
 
