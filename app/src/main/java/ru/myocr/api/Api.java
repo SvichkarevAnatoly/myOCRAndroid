7693acb96c964e423563111233b07f89b39e4c4f
Santaev Pavel <santaevp@gmail.com>
2017-03-05 21:46:45 +0700

    feature add get all cities api method

--- app/src/main/java/ru/myocr/api/Api.java
+++ app/src/main/java/ru/myocr/api/Api.java
@@ -1,16 +1,19 @@
 package ru.myocr.api;
 
 
+import java.util.List;
+
 import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
 
 public interface Api {
 
-    String SERVER_URL_LOCAL = "http://193.169.0.103:8080/";
+    String SERVER_URL_LOCAL = "http://192.168.1.105:8080/";
     String SERVER_URL_AMAZON = "http://Sample-env.323rjbfqpy.us-west-2.elasticbeanstalk.com:8080/";
 
     @POST("find")
@@ -22,4 +25,7 @@ public interface Api {
 
     @POST("add")
     Call<InsertResponse> insert(@Body InsertRequest request);
+
+    @GET("cities/all")
+    Call<List<String>> getAllCities();
 }
