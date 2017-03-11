0df413c660c540e2664542d1d1049a7e84bec869
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-03-11 17:18:29 +0300

    refactor extract method for parse price with dot

--- app/src/main/java/ru/myocr/util/PriceUtil.java
+++ app/src/main/java/ru/myocr/util/PriceUtil.java
@@ -7,14 +7,17 @@ public class PriceUtil {
     public static String getValue(ParsedPrice price) {
         final String priceValue;
         if (price.getIntValue() != null) {
-            final Integer intValue = price.getIntValue();
-            final String stringValue = intValue.toString();
-            final int priceLength = stringValue.length();
-            priceValue = stringValue.substring(0, priceLength - 2) + "." +
-                    stringValue.substring(priceLength - 2, priceLength);
+            priceValue = getStringWithDot(price.getIntValue());
         } else {
             priceValue = price.getStringValue();
         }
         return priceValue;
+    }
+
+    private static String getStringWithDot(int intValue) {
+        final String stringValue = Integer.toString(intValue);
+        final int priceLength = stringValue.length();
+        return stringValue.substring(0, priceLength - 2) + "." +
+                stringValue.substring(priceLength - 2, priceLength);
     }
 }
