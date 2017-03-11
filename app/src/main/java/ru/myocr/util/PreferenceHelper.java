1b704a8c3c16178285c23abae231ad92bf70fd07
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-03-11 13:11:50 +0300

    feature sharedPreferences as field

--- app/src/main/java/ru/myocr/util/PreferenceHelper.java
+++ app/src/main/java/ru/myocr/util/PreferenceHelper.java
@@ -7,22 +7,21 @@ import ru.myocr.model.R;
 
 public class PreferenceHelper {
 
+    public static final String KEY_CITY = "KEY_CITY";
     public static final String APP_PREFS = "APP_PREFS";
     public static final String KEY_SERVER_URL = "KEY_SERVER_URL";
-    public static final String KEY_CITY = "KEY_CITY";
+
+    private static SharedPreferences sharedPreferences = App.getAppContext().getSharedPreferences(APP_PREFS, 0);
 
     public static String getCurrentServerUrl() {
-        SharedPreferences sharedPreferences = App.getAppContext().getSharedPreferences(APP_PREFS, 0);
         return sharedPreferences.getString(KEY_SERVER_URL, App.getAppContext().getString(R.string.localhost));
     }
 
     public static void setString(String key, String value) {
-        SharedPreferences sharedPreferences = App.getAppContext().getSharedPreferences(APP_PREFS, 0);
         sharedPreferences.edit().putString(key, value).apply();
     }
 
     public static String getString(String key) {
-        SharedPreferences sharedPreferences = App.getAppContext().getSharedPreferences(APP_PREFS, 0);
         return sharedPreferences.getString(key, null);
     }
 }
