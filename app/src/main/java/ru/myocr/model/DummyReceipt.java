b5df6baadc8f7182e5e36fe12d583e4b08527857
Oydopova BS <obairka@gmail.com>
2017-03-12 23:29:08 +0700

    feature add receipt list item view

--- app/src/main/java/ru/myocr/model/DummyReceipt.java
+++ app/src/main/java/ru/myocr/model/DummyReceipt.java
@@ -16,11 +16,16 @@ public class DummyReceipt {
             Arrays.asList("Шоколад 200 гр", "Молоко простаквашино 1 л", "Мясо говядниа", "Макароны Макфа",
                     "Чай Griendfield");
 
+    private static final List<String> DUMMY_SHOPS =
+            Arrays.asList("Ашан", "Okey", "ТЦ Академгородка", "Холидей",
+                    "Быстроном");
+
+
     static {
         LIST = new ArrayList<>();
         RANDOM = new Random();
 
-        for (int i = 0; i < 3 + RANDOM.nextInt(10); i++) {
+        for (int i = 0; i < 10 + RANDOM.nextInt(10); i++) {
             LIST.add(buildItem());
         }
 
@@ -31,10 +36,10 @@ public class DummyReceipt {
         Receipt receipt = new Receipt();
 
         receipt.cashier = "Иванова Н. А";
-        receipt.date = new Date();
-        receipt.total_cost_sum = RANDOM.nextInt();
+        receipt.date = new Date(System.currentTimeMillis() - RANDOM.nextInt(100000000));
+        receipt.total_cost_sum = RANDOM.nextInt(100) * 100;
         receipt.market = new Receipt.Market();
-        receipt.market.title = "Ашан";
+        receipt.market.title = DUMMY_SHOPS.get(RANDOM.nextInt(DUMMY_SHOPS.size()));
         receipt.market.inn = "" + (100000 + RANDOM.nextInt(100000));
         receipt.market.address = "г. Новосибирск, ул. Николаева 12а";
 
