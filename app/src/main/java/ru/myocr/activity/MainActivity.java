dcc684376d3ec029b2792af3d45a9a0ad4ac028b
SvichkarevAnatoly <svichkarev.anatoly@gmail.com>
2017-03-12 16:21:40 +0300

    fix work as before

--- app/src/main/java/ru/myocr/activity/MainActivity.java
+++ app/src/main/java/ru/myocr/activity/MainActivity.java
@@ -4,10 +4,15 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.theartofdev.edmodo.cropper.CropImage;
@@ -18,23 +23,33 @@ import java.util.List;
 import ru.myocr.R;
 import ru.myocr.api.ApiHelper;
 import ru.myocr.databinding.ActivityMainBinding;
+import ru.myocr.fragment.TicketFragment;
 import ru.myocr.preference.Preference;
-import ru.myocr.preference.Server;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
-    public static final String TAG = "myOcr";
     private static final int PICK_IMAGE_REQUEST = 1;
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        handleIncomingImage(getIntent());
-        updateServerButtonText();
+        findViewById(R.id.fabCam).setOnClickListener(v -> onClickAddCam());
+        findViewById(R.id.fabGallery).setOnClickListener(v -> onClickAddGallery());
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ApiHelper.makeApiRequest(null, ApiHelper::getAllCities,
                 throwable -> {
@@ -43,18 +58,121 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void onLoadCities(List<String> cities) {
-        binding.spinnerCities.setAdapter(
-                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, cities));
-        binding.spinnerCities.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Preference.setString(Preference.CITY, cities.get(position));
-            }
+        Preference.setString(Preference.CITY, cities.get(0));
+    }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.my_tickets) {
+            openFragment(TicketFragment.newInstance());
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void openFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+    }
+
+    private void onClickAddGallery() {
+        /*Intent intent = new Intent(this, TicketParserActivity.class);
+        startActivity(intent);*/
+        selectImage();
+    }
+
+    private void onClickAddCam() {
+        /*Intent intent = new Intent(this, TicketParserActivity.class);
+        startActivity(intent);*/
+        runCamScanner();
+    }
+
+    // old Activity
+    private void handleIncomingImage(Intent intent) {
+        if (intent != null) {
+            final String type = intent.getType();
+            final Uri imageUri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+            if (type != null && type.startsWith("image/") && imageUri != null) {
+                startCropImageActivity(imageUri);
             }
-        });
+        }
+    }
+
+    private void startCropImageActivity(Uri imageUri) {
+        final CropImage.ActivityBuilder activityBuilder = CropImage.activity(imageUri)
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setMaxZoom(16)
+                .setMinCropWindowSize(0, 0) // remove minimum restriction
+                .setBorderCornerThickness(0) // remove border corners
+                .setMultiTouchEnabled(true);
+        final Intent intent = activityBuilder.getIntent(this);
+        intent.setClass(this, CropActivity.class);
+        startActivityForResult(intent, CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleIncomingImage(intent);
+    }
+
+    public void runCamScanner() {
+        Intent intent = new Intent("com.intsig.camscanner.NEW_DOC");
+        startActivity(intent);
+    }
+
+    public void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        // Always show the chooser (if there are multiple options available)
+        final Intent chooser = Intent.createChooser(intent, "Select Picture");
+        startActivityForResult(chooser, PICK_IMAGE_REQUEST);
     }
 
     @Override
@@ -72,68 +190,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        handleIncomingImage(intent);
-    }
-
-    public void runCamScanner(View view) {
-        Intent intent = new Intent("com.intsig.camscanner.NEW_DOC");
-        startActivity(intent);
-    }
-
-    public void selectImage(View view) {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        // Always show the chooser (if there are multiple options available)
-        final Intent chooser = Intent.createChooser(intent, "Select Picture");
-        startActivityForResult(chooser, PICK_IMAGE_REQUEST);
-    }
-
-    public void goToReceiptDirectly(View view) {
-        String gigantProducts = getString(R.string.test_south_products);
-        startActivityWithText(gigantProducts);
-
-        String gigantPrices = getString(R.string.test_gigant_prices);
-        startActivityWithText(gigantPrices);
-    }
-
-    public void changeServer(View view) {
-        Server.change();
-        updateServerButtonText();
-    }
-
-    private void updateServerButtonText() {
-        binding.buttonStartChangeServer.setText("Server: " + (Server.isLocal() ? "LOCAL" : "AMAZON"));
-    }
-
-    private void startCropImageActivity(Uri imageUri) {
-        final CropImage.ActivityBuilder activityBuilder = CropImage.activity(imageUri)
-                .setGuidelines(CropImageView.Guidelines.ON)
-                .setMaxZoom(16)
-                .setMinCropWindowSize(0, 0) // remove minimum restriction
-                .setBorderCornerThickness(0) // remove border corners
-                .setMultiTouchEnabled(true);
-        final Intent intent = activityBuilder.getIntent(this);
-        intent.setClass(this, CropActivity.class);
-        startActivityForResult(intent, CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE);
-    }
-
-    private void startActivityWithText(String text) {
-        Intent intent = new Intent(this, ReceiptOcrActivity.class);
-        intent.putExtra(Intent.EXTRA_TEXT, text);
-        startActivity(intent);
-    }
-
-    private void handleIncomingImage(Intent intent) {
-        if (intent != null) {
-            final String type = intent.getType();
-            final Uri imageUri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
-            if (type != null && type.startsWith("image/") && imageUri != null) {
-                startCropImageActivity(imageUri);
-            }
-        }
-    }
 }
---
+++ app/src/main/java/ru/myocr/activity/MainActivity3.java
@@ -0,0 +1,133 @@
+package ru.myocr.activity;
+
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import ru.myocr.R;
+import ru.myocr.databinding.ActivityMainBinding;
+import ru.myocr.preference.Server;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    public static final String TAG = "myOcr";
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main3);
+
+        handleIncomingImage(getIntent());
+        updateServerButtonText();
+
+        /*ApiHelper.makeApiRequest(null, ApiHelper::getAllCities,
+                throwable -> {
+                },
+                this::onLoadCities, null);*/
+    }
+
+    /*private void onLoadCities(List<String> cities) {
+        binding.spinnerCities.setAdapter(
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, cities));
+        binding.spinnerCities.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Preference.setString(Preference.CITY, cities.get(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }*/
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case PICK_IMAGE_REQUEST:
+                    if (data != null && data.getData() != null) {
+                        final Uri imageUri = data.getData();
+                        Toast.makeText(MainActivity3.this, imageUri.toString(), Toast.LENGTH_LONG).show();
+                        startCropImageActivity(imageUri);
+                    }
+                    break;
+            }
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleIncomingImage(intent);
+    }
+
+    public void runCamScanner(View view) {
+        Intent intent = new Intent("com.intsig.camscanner.NEW_DOC");
+        startActivity(intent);
+    }
+
+    public void selectImage(View view) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        // Always show the chooser (if there are multiple options available)
+        final Intent chooser = Intent.createChooser(intent, "Select Picture");
+        startActivityForResult(chooser, PICK_IMAGE_REQUEST);
+    }
+
+    public void goToReceiptDirectly(View view) {
+        String gigantProducts = getString(R.string.test_south_products);
+        startActivityWithText(gigantProducts);
+
+        String gigantPrices = getString(R.string.test_gigant_prices);
+        startActivityWithText(gigantPrices);
+    }
+
+    public void changeServer(View view) {
+        Server.change();
+        updateServerButtonText();
+    }
+
+    private void updateServerButtonText() {
+        // binding.buttonStartChangeServer.setText("Server: " + (Server.isLocal() ? "LOCAL" : "AMAZON"));
+    }
+
+    private void startCropImageActivity(Uri imageUri) {
+        final CropImage.ActivityBuilder activityBuilder = CropImage.activity(imageUri)
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setMaxZoom(16)
+                .setMinCropWindowSize(0, 0) // remove minimum restriction
+                .setBorderCornerThickness(0) // remove border corners
+                .setMultiTouchEnabled(true);
+        final Intent intent = activityBuilder.getIntent(this);
+        intent.setClass(this, CropActivity.class);
+        startActivityForResult(intent, CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE);
+    }
+
+    private void startActivityWithText(String text) {
+        Intent intent = new Intent(this, ReceiptOcrActivity.class);
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        startActivity(intent);
+    }
+
+    private void handleIncomingImage(Intent intent) {
+        if (intent != null) {
+            final String type = intent.getType();
+            final Uri imageUri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
+            if (type != null && type.startsWith("image/") && imageUri != null) {
+                startCropImageActivity(imageUri);
+            }
+        }
+    }
+}
