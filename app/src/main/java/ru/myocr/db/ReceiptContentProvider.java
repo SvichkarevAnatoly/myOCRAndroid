7e952fa87d0cf02e4dfa5203a1c6ba74bc0f142c
Santaev Pavel <santaevp@gmail.com>
2017-03-13 23:51:51 +0700

    feature add db for receipt and receipt items

---
+++ app/src/main/java/ru/myocr/db/ReceiptContentProvider.java
@@ -0,0 +1,38 @@
+package ru.myocr.db;
+
+import com.google.gson.Gson;
+
+import nl.littlerobots.cupboard.tools.gson.GsonFieldConverter;
+import nl.littlerobots.cupboard.tools.provider.CupboardContentProvider;
+import nl.qbusict.cupboard.Cupboard;
+import nl.qbusict.cupboard.CupboardBuilder;
+import ru.myocr.BuildConfig;
+import ru.myocr.model.Receipt;
+import ru.myocr.model.ReceiptItem;
+
+import static nl.qbusict.cupboard.CupboardFactory.cupboard;
+import static nl.qbusict.cupboard.CupboardFactory.setCupboard;
+
+public class ReceiptContentProvider extends CupboardContentProvider {
+
+    public static final String AUTHORITY = BuildConfig.APPLICATION_ID + ".provider";
+    public static final int DATABASE_VERSION = 1;
+
+
+    static {
+        Cupboard cupboard = new CupboardBuilder()
+                .useAnnotations()
+                .registerFieldConverter(Receipt.Market.class,
+                        new GsonFieldConverter<>(new Gson(), Receipt.Market.class))
+                .build();
+
+        setCupboard(cupboard);
+
+        cupboard().register(Receipt.class);
+        cupboard().register(ReceiptItem.class);
+    }
+
+    public ReceiptContentProvider() {
+        super(AUTHORITY, DATABASE_VERSION);
+    }
+}
