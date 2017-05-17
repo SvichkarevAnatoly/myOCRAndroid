1084283cf94bd2377f5c137cc57a14cf5237eefc
Santaev Pavel <santaevp@gmail.com>
2017-05-17 23:38:06 +0700

    feature mock receipt items before load from db

--- app/src/main/java/ru/myocr/fragment/TicketRecyclerViewAdapter.java
+++ app/src/main/java/ru/myocr/fragment/TicketRecyclerViewAdapter.java
@@ -3,9 +3,13 @@ package ru.myocr.fragment;
 import android.content.Context;
 import android.database.Cursor;
 import android.databinding.DataBindingUtil;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
@@ -16,13 +20,14 @@ import ru.myocr.App;
 import ru.myocr.R;
 import ru.myocr.databinding.ReceiptListItemBinding;
 import ru.myocr.model.Receipt;
-import ru.myocr.util.CursorRecyclerViewAdapter;
+import ru.myocr.util.MockCursorRecyclerViewAdapter;
 
 import static nl.qbusict.cupboard.CupboardFactory.cupboard;
 
-public class TicketRecyclerViewAdapter extends CursorRecyclerViewAdapter<TicketRecyclerViewAdapter.ViewHolder> {
+public class TicketRecyclerViewAdapter extends MockCursorRecyclerViewAdapter<TicketRecyclerViewAdapter.ViewHolder> {
 
     private final TicketFragment.TicketFragmentInteractionListener mListener;
+    private int lastPosition = -1;
 
     public TicketRecyclerViewAdapter(Context context, Cursor cursor,
                                      TicketFragment.TicketFragmentInteractionListener mListener) {
@@ -44,6 +49,27 @@ public class TicketRecyclerViewAdapter extends CursorRecyclerViewAdapter<TicketR
 
     @Override
     public void onBindViewHolder(TicketRecyclerViewAdapter.ViewHolder holder, Cursor cursor) {
+        if (cursor == null) {
+            holder.binding.market.setText("                         ");
+            holder.binding.date.setText("        ");
+            holder.binding.sum.setText("            ");
+            int color = Color.parseColor("#dddddd");
+            holder.binding.market.setBackgroundColor(color);
+            holder.binding.date.setBackgroundColor(color);
+            holder.binding.sum.setBackgroundColor(color);
+
+            holder.binding.getRoot().setBackgroundColor(App.getContext().getResources()
+                    .getColor(holder.getLayoutPosition() % 2 == 0
+                            ? R.color.receipt_item_bg_light
+                            : R.color.receipt_item_bg_dark));
+            holder.isMock = true;
+            return;
+        } else {
+            holder.binding.market.setBackgroundColor(Color.parseColor("#00000000"));
+            holder.binding.date.setBackgroundColor(Color.parseColor("#00000000"));
+            holder.binding.sum.setBackgroundColor(Color.parseColor("#00000000"));
+        }
+
         Receipt receipt = cupboard().withCursor(cursor).get(Receipt.class);
         receipt.loadReceiptItems(App.getContext());
 
@@ -76,6 +102,15 @@ public class TicketRecyclerViewAdapter extends CursorRecyclerViewAdapter<TicketR
                 .getColor(cursor.getPosition() % 2 == 0
                         ? R.color.receipt_item_bg_light
                         : R.color.receipt_item_bg_dark));
+
+        if (!holder.isAnimated) {
+            if (holder.isMock) {
+                setAnimation(holder.binding.layout, cursor.getPosition());
+                holder.isMock = false;
+            } else {
+                setAnimation(holder.binding.getRoot(), cursor.getPosition());
+            }
+        }
     }
 
     @Override
@@ -85,16 +120,33 @@ public class TicketRecyclerViewAdapter extends CursorRecyclerViewAdapter<TicketR
         return new ViewHolder(binding);
     }
 
+    @Override
+    public void onViewDetachedFromWindow(ViewHolder holder) {
+        holder.clearAnimation();
+        super.onViewDetachedFromWindow(holder);
+    }
+
+    private void setAnimation(View viewToAnimate, int position) {
+        Animation animation = AnimationUtils.loadAnimation(App.getContext(), R.anim.fade_in);
+        viewToAnimate.startAnimation(animation);
+    }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         public Receipt mItem;
+        private boolean isMock = false;
+        private boolean isAnimated = false;
         private ReceiptListItemBinding binding;
+
 
         public ViewHolder(ReceiptListItemBinding binding) {
             super(binding.getRoot());
 
             this.binding = binding;
+        }
+
+        public void clearAnimation() {
+            binding.getRoot().clearAnimation();
         }
 
     }
